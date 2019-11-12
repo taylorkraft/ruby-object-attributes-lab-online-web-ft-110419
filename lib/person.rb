aa55af9ab339274491ea4261a6ba1da0c1c2236c
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-12 02:40:38 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_type)
+    @job = job_type
+  end
+  
+  def job
+    @job
+  end
+end 
